6f0379888f865176900ba55fadf3036675cc8978
Jonas Gerdes <hello@jonasgerdes.com>
2016-10-02 21:41:34 +0200

    Include and use logger

--- schaubot.php
+++ schaubot.php
@@ -1,11 +1,15 @@
 <?php
     require 'vendor/autoload.php';
 
+    use Katzgrau\KLogger\Logger;
     use Telegram\Bot\Api;
+
+    $logger = new Logger(__DIR__.'/logs');
 
     $config = parse_ini_file('config.ini', true);
 
     $telegram = new Api($config['telegram']['token']);
 
     
+
 ?>
