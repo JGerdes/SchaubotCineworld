d4dc41db6569f2d98e7867d079ab157dd43e0078
Jonas Gerdes <hello@jonasgerdes.com>
2016-10-03 03:12:24 +0200

    Pretty print infos

--- src/SchauBot.php
+++ src/SchauBot.php
@@ -23,14 +23,27 @@ class SchauBot {
 
 	    $updates = $telegram->getWebhookUpdates();
 	    $movie = $this->entityManager->find('JGerdes\SchauBot\Entity\Movie', 1);
+	    $text = $this->generateMovieText($movie);
 	    if($updates->getMessage() != null) {
 		    $chatId = $updates->getMessage()->getChat()->getId();
 		    $response = $telegram->sendMessage([
 		    	'chat_id' => $chatId, 
-		    	'text' => $movie->getTitle()
+		    	'text' => $text,
+		    	'parse_mode' => 'HTML'
 			]);
 		}
+	}
 
+	private function generateMovieText($movie) {
+		$hourglas = "\xE2\x8C\x9B";
+		$desc = $movie->getDescription();
+		$desc = str_replace("<br>", "\n", $desc);
+		$desc = utf8_encode(strip_tags($desc));
+		$text = "<b>".$movie->getTitle()."</b>\n"
+			."<i>".$hourglas.$movie->getDuration()." min</i> &#183; ab "
+			.$movie->getContentRating()."\n\n"
+			.$desc;
+		return $text;
 	}
 }
 
