1022ef86776c8889fd4491762299b32d29f4dfde
Jonas Gerdes <dev@jonasgerdes.com>
2016-11-01 21:25:59 +0100

    Always generate doctrine proxies

--- bootstrap.php
+++ bootstrap.php
@@ -7,17 +7,18 @@ use Doctrine\ORM\EntityManager;
 
 $config = parse_ini_file('config.ini', true);
 
-$paths = array(__DIR__."/src");
+$paths = array(__DIR__ . "/src");
 $isDevMode = ($config['bot']['devMode'] == 'true');
 
 $dbParams = array(
-    'driver'   => $config['db']['driver'],
-    'user'     => $config['db']['user'],
+    'driver' => $config['db']['driver'],
+    'user' => $config['db']['user'],
     'password' => $config['db']['password'],
-    'dbname'   => $config['db']['dbname'],
+    'dbname' => $config['db']['dbname'],
 );
 
 $dbconfig = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$dbconfig->setAutoGenerateProxyClasses(Doctrine\Common\Proxy\AbstractProxyFactory::AUTOGENERATE_ALWAYS);
 $entityManager = EntityManager::create($dbParams, $dbconfig);
 
 ?>
