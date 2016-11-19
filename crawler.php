634f8057d209f8a3004fe5cdae8b7090070a64a5
Jonas Gerdes <dev@jonasgerdes.com>
2016-11-19 21:45:16 +0100

    Log crawler errors to file

--- crawler.php
+++ crawler.php
@@ -5,6 +5,10 @@ require_once "bootstrap.php";
 use JGerdes\SchauBot\Crawler\Crawler;
 use JGerdes\SchauBot\Crawler\Parser\TimeTableParser;
 
+//log errors in file
+ini_set('error_log', 'logs/crawler_errors.txt');
+ini_set('log_errors', true);
+
 $parser = new TimeTableParser();
 $crawler = new Crawler($config, $entityManager, $parser);
 $crawler->crawl();
